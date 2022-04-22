5a2d198a1ddcef17203e17948f9a2525bc52d942
jerome <jerometagubanoone@gmail.com>
2022-04-22 21:17:52 +0800

    Rebuild the calculator object

--- script.js
+++ script.js
@@ -1,97 +1,36 @@
 
+const CreateCalculator = () => {
+    let value = 0;
+    let hasDecimal = false;
+    let hasFirstOperator = false;
 
-const calculator = {
-    displayValue: '',
-    inputValue: '',
-    waitForSecondOperand: false,
-    operator: '',
-    hasDecimal: false,
-}
-
-const display1 = document.querySelector('.display1');
-const display2 = document.querySelector('.display2');
-
-function chooseOperation(operation) {
-
-    if (calculator.waitForSecondOperand == false){
-        calculator.waitForSecondOperand = true;
-        calculator.displayValue = calculator.inputValue;
-        console.log(calculator.inputValue);
-        calculator.inputValue = '';
-        calculator.operator = operation;
-        updateInput();
-    } else { 
-        updateDisplay();
-    }
-    calculator.operator = operation;
-    updateInput();
-}
-
-function updateDisplay() {
-
-    const num1 = parseFloat(calculator.displayValue);
-    const num2 = parseFloat(calculator.inputValue);
-
-    //if by any chance num1 becomes not a number due to del or clr
-    if (isNaN(num1)) {
-        clearDisplay();
-        return;
+    const add = (input) => {
+        value = value + input;
     }
 
-    //not allowing to compute if input is still empty
-    if (isNaN(num2)) return;    
-
-    calculator.inputValue = '';
-
-    if (calculator.operator == '*'){
-        calculator.displayValue = num1 * num2;
-    } else if (calculator.operator == '/'){
-        calculator.displayValue = num1 / num2;
-    } else if (calculator.operator == '+'){
-        calculator.displayValue = num1 + num2;
-    } else if (calculator.operator == '-'){
-        calculator.displayValue = num1 - num2;
+    const minus = (input) => {
+        value = value - input;
     }
 
-    display1.innerText = `${calculator.displayValue}`;
-    display2.innerText = `${calculator.inputValue}`;
-}
-
-function appendInput(value) {
-
-    if (value == '.') {
-        if (calculator.hasDecimal == true) return;
-        calculator.hasDecimal = true;
-    }
-    calculator.inputValue = calculator.inputValue + value;
-    updateInput();
-
-}
-
-function updateInput() {
-    display1.innerText = `${calculator.displayValue} ${calculator.operator}`;
-    display2.innerText = `${calculator.inputValue}`;
-}
-
-function clearDisplay() {
-    calculator.displayValue = '';
-    calculator.inputValue = '';
-    calculator.waitForSecondOperand = false;
-    calculator.operator = '';
-
-    updateInput();
-
-}
-
-function deleteLast() {
-
-    const len = calculator.inputValue.length;
-
-    if (len <= 0) {
-        calculator.displayValue = '';
-        calculator.waitForSecondOperand = false;
+    const multiply = (input) => {
+        value = value * input;
     }
 
-    calculator.inputValue = calculator.inputValue.substring(0, len - 1);
-    updateInput();
+    const divide = (input) => {
+        if (input == 0) return undefined;
+        value = value / input;
+    }
+
+    const clear = () => {
+        value = 0;
+        hasDecimal = false;
+        hasFirstOperator = false;
+    }
+
+    return Object.freeze({ value, add, minus, multiply, divide, clear});
 }
+
+const calculator = CreateCalculator();
+
+calculator.add(1);
+console.log(calculator.value);
